e80d0dbf5a93709feaec89f285e7b5fa0fd4be9f
Rumen Petrov <rumenpetrow@gmail.com>
2019-06-29 23:21:14 +0300

    Add functions that builds calendar

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 import './components/my-holiday.js'
 
+import { renderMoth } from './calendar.js'
+
 const API_KEY = 'AIzaSyAs7jRn_U2YncuhCAgefwSaHyupybF5cF4'
 const CALENDAR_ID = encodeURIComponent('bg.bulgarian#holiday@group.v.calendar.google.com')
 const API = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
-const elRoot = document.getElementById('root')
+
+const elHolidays = document.getElementById('js-holidays')
+const elCalendar = document.getElementById('js-calendar')
+const choosenYear = new Date().getFullYear()
 
 fetch(API)
   .then(response => response.json())
   .then(result => {
     result.items.forEach(item => {
-      if (item.start.date.includes('2019')) {
+      if (item.start.date.includes(choosenYear)) {
         const elHoliday = document.createElement('my-holiday')
 
         elHoliday.date = item.start.date
         elHoliday.title = item.summary
 
-        elRoot.appendChild(elHoliday)
+        elHolidays.appendChild(elHoliday)
       }
     })
   })
   .catch(err => {
     console.error(err)
   })
+
+for (var month = 1; month <= 12; month++) {
+  elCalendar.appendChild(renderMoth(month, choosenYear))
+}
---
+++ calendar.js
@@ -0,0 +1,101 @@
+// 1 - Monday
+// 2 - Tuesday
+// 3 - Wednesday
+// 4 - Thursday
+// 5 - Friday
+// 6 - Saturday
+// 7 - Sunday
+const getFirstDayOfMonthIndex = (month, year) => {
+  const index = new Date(year, month - 1).getDay()
+  let realIndex = 1
+
+  if (index === 0) {
+    realIndex = 7
+  } else {
+    realIndex = index
+  }
+
+  return realIndex
+}
+
+const getTotalDaysInMonth = (month, year) => {
+  return 32 - new Date(year, month - 1, 32).getDate()
+}
+
+const composeMonthData = (choosenMonth, choosenYear) => {
+  const firstDayIndex = getFirstDayOfMonthIndex(choosenMonth, choosenYear)
+  const totalDays = getTotalDaysInMonth(choosenMonth, choosenYear)
+  const data = [[], [], [], [], [], []]
+
+  for (let i = 1; i < firstDayIndex; i++) {
+    data[0].push('-')
+  }
+
+  for (let dayOfMonth = 1; dayOfMonth <= totalDays; dayOfMonth++) {
+    if (data[0].length < 7) {
+      data[0].push(dayOfMonth)
+    } else if (data[1].length < 7) {
+      data[1].push(dayOfMonth)
+    } else if (data[2].length < 7) {
+      data[2].push(dayOfMonth)
+    } else if (data[3].length < 7) {
+      data[3].push(dayOfMonth)
+    } else if (data[4].length < 7) {
+      data[4].push(dayOfMonth)
+    } else if (data[5].length < 7) {
+      data[5].push(dayOfMonth)
+    }
+  }
+
+  while (data[4].length < 7) {
+    data[4].push('+')
+  }
+
+  while (data[5].length < 7) {
+    data[5].push('+')
+  }
+
+  return data
+}
+
+const renderMoth = (choosenMonth, choosenYear) => {
+  const tableHeadData = ['П', 'В', 'С', 'Ч', 'П', 'С', 'Н']
+  const tableBodyData = composeMonthData(choosenMonth, choosenYear)
+  const elTable = document.createElement('table')
+  const elThead = document.createElement('thead')
+  const elTbody = document.createElement('tbody')
+
+  tableHeadData.map(itemValue => {
+    const elCell = document.createElement('th')
+
+    elCell.textContent = itemValue
+
+    elThead.appendChild(elCell)
+  })
+
+  tableBodyData.map(week => {
+    const elWeek = document.createElement('tr')
+
+    week.map(day => {
+      const elCell = document.createElement('td')
+
+      elCell.textContent = day
+
+      elWeek.appendChild(elCell)
+    })
+
+    elTbody.appendChild(elWeek)
+  })
+
+  elTable.appendChild(elThead)
+  elTable.appendChild(elTbody)
+
+  return elTable
+}
+
+export {
+  getFirstDayOfMonthIndex,
+  getTotalDaysInMonth,
+  composeMonthData,
+  renderMoth
+}
