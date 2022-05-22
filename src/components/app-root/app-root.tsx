665101503d9afc6f97d1ace594f020946758d87d
Rumen Petrov <rumenpetrow@gmail.com>
2022-05-22 19:36:15 +0300

    feature/#17 Revert router changes

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -14,7 +14,7 @@ export class AppRoot {
         </header>
 
         <main>
-          <stencil-router root="/vacations-calendar">
+          <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
               <stencil-route url="/" component="app-home" exact={true} />
               <stencil-route url="/profile/:name" component="app-profile" />
